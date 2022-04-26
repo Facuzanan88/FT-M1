6c1ba878fc15e013f42f775dbe6f38d22c0787ae
Facuzanan8 <facundozanandrea@gmail.com>
2022-04-26 16:09:16 -0300

    first Homework IntroToCS

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,30 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  /*
+let decimal = 0;
+let numArray = num.split('');
+for (let i = 0; i < numArray.length; i++) {
+  decimal += numArray[i] * 2 ** (numArray.length - 1 - i); 
+}
+return decimal;
+}
+*/
+return num.split('').reverse().map(function(element, index) {
+  return element * Math.pow(2, index);
+}).reduce( (a,b) => a + b);
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+let numero = num;
+let array = [];
+while (numero > 0) {
+  array.unshift(numero % 2);
+  numero = Math.floor(numero / 2);
+ 
+}
+return array.join('');
 }
 
 
