48161a99201180d44244b2ef1d3bd3580cb9de04
Facuzanan8 <facundozanandrea@gmail.com>
2022-05-18 19:32:49 -0300

    Homework part three (true) - Estructura de Datos III

--- 05-EstructuraDeDatos-III/homework/homework.js
+++ 05-EstructuraDeDatos-III/homework/homework.js
@@ -8,9 +8,88 @@
 // corre depth-first (en recorrido "post-order" cuando depthFirstForEach() es ejecutado con la opcion "post-order"
 // corre breadth-first cuando breadthFirstForEach() es ejecutado
 // Observar imagen de la carpeta "homework" llamada "bst.png". Allí encontraran dibujado el arbol utilizado para los tests
-function BinarySearchTree() {
-
+function BinarySearchTree(value) {
+  this.value = value;
+  this.right = null;
+  this.left = null;
 }
+
+BinarySearchTree.prototype.insert = function (value) {
+  if (value < this.value) {
+    if (!this.left) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+  if (value > this.value) {
+    if (!this.right) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+}
+
+BinarySearchTree.prototype.contains = function (value) {
+  if (this.value === value) return true;
+  if (value < this.value) {
+    if (!this.left) return false;
+    else return this.left.contains(value)
+  }
+  if (value > this.value) {
+    if (!this.right) return false;
+    else return this.right.contains(value)
+  }
+}
+
+BinarySearchTree.prototype.size = function () {
+  if (this.right === null && this.left === null) return 1;
+  if (this.right !== null && this.left === null) return 1 + this.right.size();
+  if (this.right === null && this.left !== null) return 1 + this.left.size();
+  if (this.right !== null && this.left !== null) return 1 + this.right.size() + this.left.size();
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function (cb, order) {
+  if (order === 'pre-order') { // rot - izq - der
+    cb(this.value)
+    if (this.left !== null) this.left.depthFirstForEach(cb, order)
+    if (this.right !== null) this.right.depthFirstForEach(cb, order)
+  }
+  else if (order === 'post-order') { // izq - der - rot
+    if (this.left !== null) this.left.depthFirstForEach(cb, order)
+    if (this.right !== null) this.right.depthFirstForEach(cb, order)
+    cb(this.value)
+  }
+  else { // order = in-order  -> izq - rot - der
+    if (this.left !== null) this.left.depthFirstForEach(cb, order)
+    cb(this.value)
+    if (this.right !== null) this.right.depthFirstForEach(cb, order)
+  }
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function (cb, array = []) {
+if (this.left !== null) array.push(this.left);
+if (this.right !== null) array.push(this.right);
+cb(this.value);
+if (array.length !== 0) array.shift().breadthFirstForEach (cb, array);
+}
+
+
+// tambien se puede resolver tmb con el metodo 'Switch' a 'depthFirstForEach' 
+/*  Switch(order) {
+   case 'pre-order':
+     ///
+     break;
+     case 'post-order'
+     ///
+     break;
+     default: // in-order
+     ///
+     break;
+ }
+ } */
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
